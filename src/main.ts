2ceaed92c71c6747d5d60b9c9d045a1179df24b9
Dev <dev@AZTs-MacBook-Air.local>
2024-07-10 00:02:19 +0500

    cors added

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000'], // Allowed origins
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
+      allowedHeaders: 'Content-Type,Authorization', // Allowed headers
+      credentials: true, // Enable cookies to be sent across origins
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(8000);
 }
